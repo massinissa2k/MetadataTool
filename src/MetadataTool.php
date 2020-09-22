789ccc7ccd2236def954bb83fb5872470566b55e
HAMIDOUCHE MASSINISSA <M.HAMIDOUCHE>
2020-09-22 16:47:29 +0200

    Cmp info

--- src/MetadataTool.php
+++ src/MetadataTool.php
@@ -2,10 +2,74 @@
 
 namespace Massinissa\MetadataTool;
 
+/**
+ * Metadata des fichiers PDF et IMAGES, permet aussi de structurer les meta dans le cadre de santiane (metier)
+ * Class MetadataTool
+ *
+ * @package Massinissa\MetadataTool
+ */
 class MetadataTool
 {
-    public function __construct()
+    /** @var string */
+    protected $filename;
+
+    /**
+     * @var bool
+     */
+    protected $isBase64 = false;
+
+    public function __construct(string $filename)
     {
-        dump('OK 2');
+        $this->setFilename($filename);
+    }
+
+    /**
+     * @return bool
+     */
+    public function addMetadata(): bool
+    {
+        $success = PDF_set_info($this->getFilename(), 'isMyKey', 'isMyValue');
+
+        return $success;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFilename(): string
+    {
+        return $this->filename;
+    }
+
+    /**
+     * @param string $filename
+     *
+     * @return $this
+     */
+    public function setFilename(string $filename): self
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBase64(): bool
+    {
+        return $this->isBase64;
+    }
+
+    /**
+     * @param bool $isBase64
+     *
+     * @return MetadataTool
+     */
+    public function setIsBase64(bool $isBase64): MetadataTool
+    {
+        $this->isBase64 = $isBase64;
+
+        return $this;
     }
 }
